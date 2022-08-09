286c86012cf9d3c142389879cdc3a881c6294c54
hanasay <jerrytommy2009@gmail.con>
2022-08-09 11:12:00 +0800

    B C狀態待

--- Assets/Script/Shura/ShuraMovement.cs
+++ Assets/Script/Shura/ShuraMovement.cs
@@ -7,6 +7,17 @@ public class ShuraMovement : MonoBehaviour
 {
     public GameObject aModeAttack;
     public GameObject bModeAttack;
+    public GameObject appModeAttack;
+    public GameObject bppModeAttack;
+
+    //常數
+    float timer;
+    float delayTime;
+    float IdleTime;
+    float currentDistance;
+    float randAttackMode;
+    float randCoin;
+    public GameObject targetPlayer;
 
     public List<GameObject> triggerPrefab = new List<GameObject>();
 
@@ -14,30 +25,34 @@ public class ShuraMovement : MonoBehaviour
     public float health = 100f;
     float maxHealth;
     [SerializeField]
-    State BossState = State.Idle;
+    State BossState = State.Sleep;
 
-    public float speed;
-    public float Idle_time;
+    public float aModeDelay;    //A狀態的攻擊前延遲
+    public float aSpeed;        //A攻擊的上升速度
+    public float bModeDelay;    //B狀態的攻擊前延遲
+    public float bSpeed;        //B攻擊的子彈飛行速度
+    public float bTimer;        //B攻擊的子彈飛行時間
+    public float cModeDelay;
+    public float cTimer;
+    public float rushSpeed;     //C狀態的衝刺速度
+    public float rushRadius;
+    public float stage;         //用以辨識階段三的運行階段
+
+    //Agent reference
     public NavMeshAgent agent;
-    public float detectRadius;
-    public float stopDistance;
-    public GameObject targetPlayer;
-    float currentDistance = 100f;
-    float timer;
-    float delayTime;
+    public float detectRadius;  //修羅甦醒的偵測距離
+    public float walkSpeed;     //修羅的行走速度
+    public float stopDistance;  //修羅與玩家間的預設距離
 
-    float randAttackMode;
-    float randCoin;
-
-    // Start is called before the first frame update
     void Start()
     {
-        agent.speed = speed;
+        IdleTime = 2f;
+        agent.speed = walkSpeed;
         agent.stoppingDistance = stopDistance;
         maxHealth = health;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         //由血量判斷階段
@@ -72,13 +87,13 @@ public class ShuraMovement : MonoBehaviour
 
                 if (currentDistance <= stopDistance)
                 {
-                    Idle_time -= Time.deltaTime;
+                    IdleTime -= Time.deltaTime;
                     walkAround(currentDistance);
                 }
 
-                if (Idle_time <= 0)
+                if (IdleTime <= 0)
                 {
-                    Idle_time = Random.Range(1f, 5f);
+                    IdleTime = Random.Range(1f, 5f);
                     BossState = State.Attack;
                 }
                 break;
@@ -89,35 +104,23 @@ public class ShuraMovement : MonoBehaviour
                 randAttackMode = Random.Range(0f, 10f);
                 randAttackMode = (int)randAttackMode;
                 randCoin = Random.Range(0f, 4f);
-                timer -= Time.deltaTime;
                 if (phase == 0) { Debug.Log("Attack Mode: " + randAttackMode % 3); }
                 else { Debug.Log("Attack Mode: " + randAttackMode % 4); }
                 if (phase == 0)
                 {
                     if (randAttackMode % 3 == 0)
                     {
-                        delayTime = 1f;
-                        timer = 0.1f;
-                        triggerPrefab.Add(Instantiate(aModeAttack, targetPlayer.transform.position - new Vector3(0f, 2f, 0f), targetPlayer.transform.rotation));
+                        aModePrepare();
                         BossState = State.Mode_A;
                     }
                     else if (randAttackMode % 3 == 1)
                     {
-                        timer = 1f;
-                        //正前方
-                        triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * 2f, gameObject.transform.rotation));
-                        //右前方
-                        triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(22.5f) * 2f * -1 + gameObject.transform.right * Mathf.Sin(22.5f) * 2f * -1, gameObject.transform.rotation * Quaternion.Euler(0f,22.5f,0f)));
-                        //左方
-                        triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(45) * 2f + gameObject.transform.right * Mathf.Sin(45) * 2f * -1, gameObject.transform.rotation * Quaternion.Euler(0, -45f, 0)));
-                        //左前方
-                        triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(22.5f) * 2f * -1 + gameObject.transform.right * Mathf.Sin(22.5f) * 2f, gameObject.transform.rotation * Quaternion.Euler(0, -22.5f, 0)));
-                        //右方
-                        triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(45) * 2f + gameObject.transform.right * Mathf.Sin(45) * 2f, gameObject.transform.rotation * Quaternion.Euler(0, 45f, 0)));
+                        delayTime = bModeDelay;
                         BossState = State.Mode_B;
                     }
                     else
                     {
+                        delayTime = cModeDelay;
                         BossState = State.Mode_C;
                     }
                 }
@@ -127,141 +130,184 @@ public class ShuraMovement : MonoBehaviour
                     {
                         //A>B
                         BossState = State.Mode_A;
-                        BossState = State.Mode_B;
                     }
                     else if (randAttackMode % 3 == 1)
                     {
                         //B>A mode
+                        delayTime = bModeDelay;
                         BossState = State.Mode_B;
-                        BossState = State.Mode_A;
                     }
                     else if (randAttackMode % 3 == 2)
                     {
                         //C>A mode
                         BossState = State.Mode_C;
-                        BossState = State.Mode_A;
                     }
                     else
                     {
                         //C>B mode
                         BossState = State.Mode_C;
-                        BossState = State.Mode_B;
-                    }
-
-                    if(randCoin % 2 == 0)
-                    {
-                        //Instaniate coin
                     }
                 }
 
                 else
                 {
-                    if (randAttackMode % 3 == 0)
+                    stage = 0;
+                    if (randAttackMode % 4 == 0)
                     {
                         //A>A>A+ mode
                         BossState = State.Mode_A;
-                        randCoin = Random.Range(0f, 4f);
-                        BossState = State.Mode_A;
-                        randCoin = Random.Range(0f, 4f);
-                        BossState = State.Mode_App;
-                        randCoin = Random.Range(0f, 4f);
                     }
-                    else if (randAttackMode % 3 == 1)
+                    else if (randAttackMode % 4 == 1)
                     {
                         //B>B>B+ mode
                         BossState = State.Mode_B;
-                        randCoin = Random.Range(0f, 4f);
-                        BossState = State.Mode_B;
-                        randCoin = Random.Range(0f, 4f);
-                        BossState = State.Mode_Bpp;
-                        randCoin = Random.Range(0f, 4f);
                     }
-                    else if (randAttackMode % 3 == 2)
+                    else if (randAttackMode % 4 == 2)
                     {
                         //C>A>B mode
                         BossState = State.Mode_C;
-                        randCoin = Random.Range(0f, 4f);
-                        BossState = State.Mode_A;
-                        randCoin = Random.Range(0f, 4f);
-                        BossState = State.Mode_B;
-                        randCoin = Random.Range(0f, 4f);
                     }
                     else
                     {
                         //C>B>A mode  
+                        BossState = State.Mode_C;
                     }
                 }
                 break;
 
             case State.Mode_A:
+                //攻擊前的延遲
+                if (delayTime > 0) delayTime -= Time.deltaTime;
 
-                if (delayTime > 0)
-                {
-                    delayTime -= Time.deltaTime;
-                    //Debug.Log("Time left: " + delayTime);
-                }
-
+                //判斷攻擊是否完成 (快速上升擊中玩家)
                 if (delayTime <= 0 && triggerPrefab[0].transform.position.y <= targetPlayer.transform.position.y)
-                {
-                    triggerPrefab[0].transform.position += new Vector3(0f, 20f, 0f) * Time.deltaTime;
-                }
+                    triggerPrefab[0].transform.position += new Vector3(0f, aSpeed, 0f) * Time.deltaTime;
+
+
+                //完成攻擊後的切換狀態準備
                 else if (delayTime <= 0 && triggerPrefab[0].transform.position.y >= targetPlayer.transform.position.y)
                 {
                     Debug.Log("Attack Done!");
+                    //移除A物件 以及從List中除名
                     Destroy(triggerPrefab[0]);
                     triggerPrefab.RemoveAt(0);
+
+                    //階段1 切回Idle
                     if (phase == 0) BossState = State.Idle;
+                    //階段2
                     else if (phase == 1)
                     {
+                        coinGenerator();
+                        //模式1 切至B狀態
                         if (randAttackMode % 4 == 0) BossState = State.Mode_B;
+                        //其餘切回Idle
                         else BossState = State.Idle;
                     }
                     else
                     {
-                        if (randAttackMode % 4 == 0)
-                        {
-                            delayTime = 1f;
-                            timer = 0.1f;
-                            triggerPrefab.Add(Instantiate(aModeAttack, targetPlayer.transform.position - new Vector3(0f, 2f, 0f), targetPlayer.transform.rotation));
-                        }
+                        //coin生成判斷
+                        coinGenerator();
+                        stage++;
+                        //模式1 持續執行A模式
+                        if (randAttackMode % 4 == 0) aModePrepare();
+                        //模式3 切至B狀態
                         else if (randAttackMode % 4 == 2) BossState = State.Mode_B;
+                        //其餘(只有模式4) 切回Idle
                         else BossState = State.Idle;
                     }
                 }
                 break;
 
             case State.Mode_B:
-                if (timer > 0)
+                //攻擊前的延遲
+                if(delayTime >= 0) delayTime -= Time.deltaTime;
+
+                //生成子彈
+                else if(triggerPrefab.Count == 0) bModePrepare();
+
+                //子彈飛行
+                else if (timer > 0)
                 {
                     timer -= Time.deltaTime;
                     foreach (GameObject i in triggerPrefab)
                     {
-                        i.transform.position += i.transform.forward * 5f * Time.deltaTime;
+                        i.transform.position += i.transform.forward * bSpeed * Time.deltaTime;
                     }
                 }
 
+                //完成攻擊後的切換狀態準備
                 else
                 {
                     Debug.Log("Attack Done!");
                     
+                    //移除B物件 以及從List中除名
                     for (int i = 0; i < triggerPrefab.Count; i++) Destroy(triggerPrefab[0]);
                     foreach (GameObject i in triggerPrefab) triggerPrefab.Remove(i);
-                    
+
+                    //階段1 切回Idle
                     if (phase == 0) BossState = State.Idle;
-                    else if(phase == 1)
+
+                    //階段2
+                    else if (phase == 1)
                     {
-                        if (randAttackMode % 2 == 1) BossState = State.Mode_A;
+                        coinGenerator();
+                        //模式2 切至A模式
+                        if (randAttackMode % 4 == 1) BossState = State.Mode_A;
+                        //其餘狀況 切回Idle
                         else BossState = State.Idle;
+                    }
+                    else
+                    {
+                        //coin生成判斷
+                        coinGenerator();
+                        stage++;
+
+                        //模式4 切至A狀態
+                        if (randAttackMode % 4 == 3)
+                        {
+                            aModePrepare();
+                            BossState = State.Mode_A;
+                        }
+                        //其餘狀況 運行至第三步驟時 回到Idle
+                        else if(stage == 3) BossState = State.Idle;
                     }
                 }
 
                 break;
 
             case State.Mode_C:
-                Debug.Log("Attack Done!");
-                timer = 2;
-                BossState = State.Idle;
+                if (delayTime >= 0) delayTime -= Time.deltaTime;
+                else cModePrepare();
 
+                if (timer >= 0) timer -= Time.deltaTime;
+                else
+                {
+                    agent.speed = walkSpeed;
+                    agent.stoppingDistance = stopDistance;
+                }
+                Debug.Log("Attack Done!");
+
+                if (phase == 0) BossState = State.Idle;
+                else if(phase == 1)
+                {
+                    coinGenerator();
+                    if (randAttackMode % 4 == 2)
+                    {
+                        aModePrepare();
+                        BossState = State.Mode_A;
+                    }
+                    else BossState = State.Mode_B;
+                }
+                else
+                {
+                    coinGenerator();
+                    if (randAttackMode % 4 == 2)
+                    {
+                        aModePrepare();
+                        BossState = State.Mode_A;
+                    }
+                    else BossState = State.Mode_B;
+                }
                 break;
 
             default:
@@ -272,14 +318,66 @@ public class ShuraMovement : MonoBehaviour
         {
             if (((int)distance / 2) % 2 == 0)
             {
-                agent.Move(gameObject.transform.right * speed / 2 * Time.deltaTime);
+                agent.Move(gameObject.transform.right * walkSpeed / 2 * Time.deltaTime);
                 //Debug.Log("Slowly moving right");
             }
             else
             {
-                agent.Move(gameObject.transform.right * -1 * speed / 2 * Time.deltaTime);
+                agent.Move(gameObject.transform.right * -1 * walkSpeed / 2 * Time.deltaTime);
                 //Debug.Log("Slowly moving left");
             }
+        }
+
+        void aModePrepare()
+        {
+            delayTime = aModeDelay;
+            timer = 0.1f;
+            if(stage == 3 && randAttackMode % 4 == 0) triggerPrefab.Add(Instantiate(appModeAttack, targetPlayer.transform.position - new Vector3(0f, 2f, 0f), targetPlayer.transform.rotation));
+            else triggerPrefab.Add(Instantiate(aModeAttack, targetPlayer.transform.position - new Vector3(0f, 2f, 0f), targetPlayer.transform.rotation));
+        }
+
+        void bModePrepare()
+        {
+            timer = bTimer;
+
+            if (stage == 3 && randAttackMode % 4 == 1)
+            {
+                //正前方
+                triggerPrefab.Add(Instantiate(bppModeAttack, gameObject.transform.position + gameObject.transform.forward * 2f, gameObject.transform.rotation));
+                //右前方
+                triggerPrefab.Add(Instantiate(bppModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(22.5f) * 2f * -1 + gameObject.transform.right * Mathf.Sin(22.5f) * 2f * -1, gameObject.transform.rotation * Quaternion.Euler(0f, 22.5f, 0f)));
+                //左方
+                triggerPrefab.Add(Instantiate(bppModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(45) * 2f + gameObject.transform.right * Mathf.Sin(45) * 2f * -1, gameObject.transform.rotation * Quaternion.Euler(0, -45f, 0)));
+                //左前方
+                triggerPrefab.Add(Instantiate(bppModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(22.5f) * 2f * -1 + gameObject.transform.right * Mathf.Sin(22.5f) * 2f, gameObject.transform.rotation * Quaternion.Euler(0, -22.5f, 0)));
+                //右方
+                triggerPrefab.Add(Instantiate(bppModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(45) * 2f + gameObject.transform.right * Mathf.Sin(45) * 2f, gameObject.transform.rotation * Quaternion.Euler(0, 45f, 0)));
+            }
+            else
+            {
+                //正前方
+                triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * 2f, gameObject.transform.rotation));
+                //右前方
+                triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(22.5f) * 2f * -1 + gameObject.transform.right * Mathf.Sin(22.5f) * 2f * -1, gameObject.transform.rotation * Quaternion.Euler(0f, 22.5f, 0f)));
+                //左方
+                triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(45) * 2f + gameObject.transform.right * Mathf.Sin(45) * 2f * -1, gameObject.transform.rotation * Quaternion.Euler(0, -45f, 0)));
+                //左前方
+                triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(22.5f) * 2f * -1 + gameObject.transform.right * Mathf.Sin(22.5f) * 2f, gameObject.transform.rotation * Quaternion.Euler(0, -22.5f, 0)));
+                //右方
+                triggerPrefab.Add(Instantiate(bModeAttack, gameObject.transform.position + gameObject.transform.forward * Mathf.Cos(45) * 2f + gameObject.transform.right * Mathf.Sin(45) * 2f, gameObject.transform.rotation * Quaternion.Euler(0, 45f, 0)));
+            }
+        }
+
+        void cModePrepare()
+        {
+            timer = cTimer;
+            agent.speed = rushSpeed;
+            agent.stoppingDistance = rushRadius;
+        }
+
+        void coinGenerator()
+        {
+            //用randCoin做生成判斷
         }
     }
 }
@@ -287,5 +385,5 @@ public class ShuraMovement : MonoBehaviour
 
 enum State
 {
-    Sleep, Idle, Attack, Mode_A, Mode_B, Mode_C, Mode_App, Mode_Bpp
+    Sleep, Idle, Attack, Mode_A, Mode_B, Mode_C
 }
